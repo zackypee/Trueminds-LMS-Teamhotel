23e4d34d29bdc64ce1f8d0e27e7af0298b43af3a
Jaysquare <jamiuolajide20@gmail.com>
2026-03-29 14:54:57 +0100

    add the reset-password-form component

---
+++ src/features/auth/components/ResetPasswordForm.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import forwardIcon from '../../assets/forward-icon.svg';
+import backwardIcon from '../../assets/backward-icon.svg';
+import emailIcon from '../../assets/email-icon.svg';    
+
+export const ResetPasswordForm = () => {    
+    return (
+        <div className="reset-password-form" role='form'>
+            <h2>Reset Your Password</h2>
+            <p>Enter your email address and we'll send you instructions to reset your password.</p>
+            <form>
+                <label htmlFor="email">Email Address</label>
+                <div className="input-container">
+                  <img src={emailIcon} alt="Email icon" />
+                  <input type="email" id="email" name="email" required placeholder="email@example.com" /> 
+                </div>
+                <button type="submit">
+                  Send Reset Link 
+                  <img src={forwardIcon} alt="Forward" />
+                </button>
+            </form>
+           <Link to="/login">
+                <img src={backwardIcon} alt="Back" />
+                Back to Login
+           </Link>
+
+           <p>Need help? <Link to="/contact">Contact TalentFlow Support</Link></p>
+        </div>
+    );
+}
