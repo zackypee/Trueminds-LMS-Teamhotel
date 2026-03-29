c949f102f7f02b85296c0bdc22f88a1258356696
Benny Melda <bennymelda@gmail.com>
2026-03-29 16:48:05 +0100

    add inbox page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Inbox from './features/auth/pages/check-inbox'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-      <div>
-        
-      </div>
+      <>
+       <Inbox /> 
+      </>
   )
 }
 
