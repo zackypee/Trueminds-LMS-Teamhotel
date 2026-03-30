6290beb1d7a0888b1b6b90dfc00f2fdf9d9e4aff
Peter <zackyypee@gmail.com>
2026-03-30 22:03:43 +0100

    Remove redundant font weight from Google Fonts link and update App component to render ResetPasswordPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
 import Footer from './components/Footer';
+import { ResetPasswordPage } from './features/auth/pages/ResetPasswordPage';
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Footer />
-      
+      <ResetPasswordPage />
     </BrowserRouter>
   )
 }
