0102956d7e10dfa54e1d10f0f20a9640a0fd263e
Peter <zackyypee@gmail.com>
2026-03-30 21:10:10 +0100

    Add Footer component to App for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <BrowserRouter>
-      
+      <Footer />
       
     </BrowserRouter>
   )
