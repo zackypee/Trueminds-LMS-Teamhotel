e12d2c5d7cad404f00b310e45d5977f1339c03ec
Jaysquare <jamiuolajide20@gmail.com>
2026-03-31 01:13:39 +0100

    adjust reset-password-page layout and stardardize tailwind approach

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ export const Button = ({children, onClick, className}) => {
     return (
         <button onClick={onClick} 
          role="button"
-         className={`px-4 py-2 bg-[#7C3AED] text-white w-[380px] rounded-md cursor-pointer ${className}`}
+         className={`px-4 py-2 bg-[#7C3AED] text-white w-95 rounded-md cursor-pointer ${className}`}
         >
           {children}
         </button>
--- src/features/auth/pages/ResetPasswordPage.jsx
+++ src/features/auth/pages/ResetPasswordPage.jsx
@@ -3,7 +3,7 @@ import { ResetPasswordForm } from "../components/ResetPasswordForm";
 
 export const ResetPasswordPage = () => {
     return (
-        <section className="reset-password-page flex flex-col gap-8 min-[900px]:flex-row  min-[900px]:gap-0 ">   
+        <section className="reset-password-page h-screen align-items flex flex-col gap-8 min-[900px]:flex-row  min-[900px]:gap-0 ">   
             <ResetPasswordBanner />
             <ResetPasswordForm />
         </section>   
