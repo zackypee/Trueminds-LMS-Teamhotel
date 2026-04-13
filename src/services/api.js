0fc117e5bfd95e4d21f0dab40934758b025c9f8f
Peter <zackyypee@gmail.com>
2026-04-13 19:35:14 +0100

    removed time out

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL:import.meta.env.VITE_API_BASE_URL,
-    timeout: 15000,
     headers:{
         "Content-Type": "application/json"
     }
