2da8236dcfc3eae3b5988efea03b93c765fd783b
Jaysquare <jamiuolajide20@gmail.com>
2026-05-06 18:04:26 +0100

    commit before merge

--- src/features/learning/userComponents/UserEditProfileModal.jsx
+++ src/features/learning/userComponents/UserEditProfileModal.jsx
@@ -72,6 +72,7 @@ export default function UserEditProfileModal({ onClose, userProfile, setRefresh 
           onSave={handleSave}
           onCancel={onClose} 
           isUpdating={isUpdating} 
+          userProfile={userProfile}
           
         />
       </div>
